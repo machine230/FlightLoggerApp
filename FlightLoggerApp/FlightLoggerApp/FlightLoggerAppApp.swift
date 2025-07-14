32f427db1bf311c078a11b0aeff8945d1b935f99
Your Name <cd>
2025-07-14 16:09:42 -0700

    Moved Xcode project into repo

---
+++ FlightLoggerApp/FlightLoggerApp/FlightLoggerAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  FlightLoggerAppApp.swift
+//  FlightLoggerApp
+//
+//  Created by Manju Mayachar on 7/14/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FlightLoggerAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
